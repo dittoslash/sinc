3ac18af49db5d6bf60a8fe3dfaf7801888888f16
Dittoslash via Git <dittoslash@gmail.com>
2015-06-13 15:08:56 +0000

    Added more functionality :)

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 var money = 0
 var moneyperclick = 1
 var mpms = 0
+var hasPermit = false
 window.onload = function () { //Loading script
 txtBox("Page Loaded, starting Sinc");
 window.setInterval(tick, 10);
+$(".permithide").hide();
 txtBox("Sinc Loaded");
 }
 function pressButton() {
@@ -12,5 +14,27 @@ function pressButton() {
 }
 function tick() {
 	money = money + mpms
-	updateMoney()
+	updateMoney();
+}
+function buyWorker(i) {
+	if (money > ((i * 100) - 1)) {
+		money = money - (i * 100);
+		addIncome(i / (11 - i));
+		txtBox("Bought worker");
+	} else {
+		txtBox("Couldn't afford");
+	}
+}
+function addIncome(i) {
+	mpms = mpms + i
+}
+function buyPermit() {
+	if (money > (500) - 1) {
+		hasPermit = true
+		$(".permithide").show();
+		$("#workerpermit").hide();
+		txtBox("Bought permit")
+	} else {
+		txtBox("Couldn't afford");
+	}
 }
