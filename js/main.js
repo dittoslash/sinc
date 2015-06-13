6759afadb1cb79dfaa9e0adc399856216a60b84e
Dittoslash via Git <dittoslash@gmail.com>
2015-06-13 19:25:04 +0000

    Super Amazing Update +Cookies +Pointer upgrade -Prices increased for workers

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
 var money = 0
 var moneyperclick = 1
 var mpms = 0
-var hasPermit = false
+var hasPermit = "false"
 window.onload = function () { //Loading script
-txtBox("Page Loaded, starting Sinc");
+txtBox("Loading");
 window.setInterval(tick, 10);
 $(".permithide").hide();
+txtBox("Loaded tick")
+money = parseFloat(Cookies.get("money"));
+moneyperclick = parseFloat(Cookies.get("moneyperclick"));
+mpms = parseFloat(Cookies.get("mpms"));
+hasPermit = Cookies.get("hasPermit")
+txtBox("Loaded cookies")
+if (isNaN(money) && isNaN(mpms) && isNaN(moneyperclick)) {
+	money = 0
+	moneyperclick = 1
+	mpms = 0
+	hasPermit = "false"
+}
+txtBox("Fixed naan bread")
+if (hasPermit == "true") {
+	$(".permithide").show();
+	$("#workerpermit").hide();
+}
+txtBox("Loaded permit cookie")
+getElById('pointerupgrade').innerHTML = "Upgrade mouse for 1000 money (current: " + moneyperclick + ")"
+txtBox("MPC display")
 txtBox("Sinc Loaded");
 }
 function pressButton() {
@@ -16,20 +36,25 @@ function tick() {
 	money = money + mpms
 	updateMoney();
 }
-function buyWorker(i) {
-	if (money > ((i * 100) - 1)) {
-		money = money - (i * 100);
+
+
+function buyWorker(i) { //WARNING MATH AHEAD >.<
+	if (money > (((i * 100) - 1) + (i * 5) + (i * 100))) {
+		money = money - (((i * 100)) + (i * 5) + (i * 100));
 		addIncome(i / (11 - i));
 		txtBox("Bought worker");
 	} else {
 		txtBox("Couldn't afford");
 	}
 }
+
+
 function addIncome(i) {
 	mpms = mpms + i
 }
 function buyPermit() {
 	if (money > (500) - 1) {
+		money = money - 500
 		hasPermit = true
 		$(".permithide").show();
 		$("#workerpermit").hide();
@@ -38,3 +63,23 @@ function buyPermit() {
 		txtBox("Couldn't afford");
 	}
 }
+function upgradeMouse() {
+	if (money > (1000) - 1) {
+		money = money - 1000
+		moneyperclick = moneyperclick + 1
+		txtBox("Upgraded pointer")
+		getElById('pointerupgrade').innerHTML = "Upgrade mouse for 1000 money (current: " + moneyperclick + ")"
+		if (moneyperclick == 20) {
+			$("#pointerupgrade").hide();
+			txtBox("Max level of pointer reached")
+		}
+	} else {
+		txtBox("Couldn't afford")
+	}
+}
+$(window).unload(function() {
+	Cookies.set("money", money);
+	Cookies.set("moneyperclick", moneyperclick);
+	Cookies.set("mpms", mpms);
+	Cookies.set("hasPermit", hasPermit)
+});
